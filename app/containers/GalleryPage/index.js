fd6a54f34915c92e3002553afa16bc0359cdaf9e
Nutchanon Phongoen <nutchanon.pho@gmail.com>
2017-11-16 20:20:37 +0700

    test new eslint

--- app/containers/GalleryPage/index.js
+++ app/containers/GalleryPage/index.js
@@ -1,34 +1,40 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import Menu from 'containers/Menu';
-import { Container, Grid, Icon } from 'semantic-ui-react';
+import { Container, Card, Image } from 'semantic-ui-react';
 import Footer from 'components/Footer';
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
+// import { FormattedMessage } from 'react-intl';
+// import messages from './messages';
 
 
 export default class GalleryPage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
       <article>
-        <Helmet>
-          <title>Shop Page</title>
+        <Helmet><title>Shop Page</title>
           <meta name="description" content="" />
         </Helmet>
         <Menu />
-        <Container style={{ marginTop: '200px', height: '300px' }}>
-          <Grid stackable divided columns={2}>
-            <Grid.Column textAlign="center">
-              <div><Icon name="add circle" size="massive" /></div>
-              <br />
-              <div><h1><FormattedMessage {...messages.upload} /></h1></div>
-            </Grid.Column>
-            <Grid.Column textAlign="center">
-              <div><Icon name="image" size="massive" /></div>
-              <br />
-              <div><h1><FormattedMessage {...messages.chooseFromGallery} /></h1></div>
-            </Grid.Column>
-          </Grid>
+        <Container style={{ marginTop: '200px' }}>
+          <Card.Group>
+            <Card>
+              <Card.Content>
+                <Image floated="right" size="mini" src="/assets/images/avatar/large/steve.jpg" />
+                <Card.Header>
+                  Steve Sanders
+                </Card.Header>
+                <Card.Meta>
+                  Friends of Elliot
+                </Card.Meta>
+                <Card.Description>
+                  Steve wants to add you to the group <strong>best friends</strong>
+                </Card.Description>
+              </Card.Content>
+              <Card.Content extra>
+                test
+              </Card.Content>
+            </Card>
+          </Card.Group>
         </Container>
         <Footer />
       </article>
